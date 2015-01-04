8729f8c50a296108c3bf8ff8c9ebd4edebc4287f
Michael <michael@iMac-de-Alexandra-2.local>
2015-01-04 11:01:48 -0500

    affichage des types de travaux

--- src/Jna/MainBundle/Controller/DefaultController.php
+++ src/Jna/MainBundle/Controller/DefaultController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Jna\MainBundle\Entity\Enquiry;
 use Jna\MainBundle\Form\EnquiryType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
         $doctrine_entity = $this->getDoctrine();
-
         $enquiry = new Enquiry();
         $form = $this->createForm(new EnquiryType(), $enquiry);
 
@@ -40,12 +40,61 @@ class DefaultController extends Controller
 
         $work_types = $doctrine_entity
             ->getRepository('mainBundle:WorkType')
-            ->findAll();
+            ->findBy(array(), array('type' => 'ASC'));
 
         return $this->render('mainBundle:Default:index.html.twig' , array(
             "projects"  => $projects,
             "workTypes" => $work_types,
             'form' => $form->createView()
         ));
+    }
+
+    public function invoiceAction(Request $request)
+    {
+        if ($request->getMethod() == 'POST') {
+
+            $customers        = $_POST['invoice_client'];
+            $customers_adress = $_POST['invoice_adress'];
+            $invoice_date     = $_POST['invoice_date'];
+            $materials        = $this->formatMaterials($_POST['material'] , $_POST['price']);
+
+            var_dump($_POST);
+            exit();
+
+            $html = $this->renderView('mainBundle:Default:invoice_pdf.html.twig', array(
+                'customers' => 'customers',
+                'customers_adress' => 'customers_adress',
+                'invoice_date'     => 'invoice_date',
+                'materiaux'        => 'materiaux',
+                'divers'           => 'divers',
+                'main_oeuvre'      => 'main_oeuvre',
+            ));
+
+            return new Response(
+                $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+                200,
+                array(
+                    'Content-Type'          => 'application/pdf',
+                    'Content-Disposition'   => 'attachment; filename="file.pdf"'
+                )
+            );
+        }
+
+        return $this->render('mainBundle:Default:invoice.html.twig');
+    }
+
+    private function formatMaterials($materials_name , $material_prices)
+    {
+        $subtotal_materials = 0;
+
+        foreach($materials_name as $key=>$mn){
+
+            $materials['recap'][] = $mn . ' - ' . $material_prices[$key] . ' $';
+            $subtotal_materials   +=  $material_prices[$key];
+        }
+
+        $materials['subtotal'] = $subtotal_materials;
+
+        return $materials;
     }
 }
---
+++ src/Jna/MainBundle/Form/InvoiceType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 15-01-02
+ * Time: 09:25
+ */
+
+namespace Jna\MainBundle\Form;
+
+
+class InvoiceType {
+
+} 
